b388830a2ecbc1a345dbdd7a4f93558420ef587f
Edu <eduardokaue277@gmail.com>
2024-04-01 22:25:47 -0300

    Atualização do Projeto

---
+++ DiceRoller/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace DiceRoller
+{
+    public partial class MainPage : ContentPage
+    {
+        int valor = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            SlidesPicker.SelectedIndex = 0;
+        }
+
+ 
+        private void RollBtn_Clicked(System.Object sender, System.EventArgs e)
+        {
+            valor = Convert.ToInt32(SlidesPicker.SelectedItem);
+            int valorSorteado = 0;
+            valorSorteado = new Random().Next(1, valor +1);
+
+            Result.Text = valorSorteado.ToString();
+            
+        }
+    }
+
+}
